c533dd11cfd2ab6a06ec2bc0236ab68d5266e31d
carinedj <ccdjager@yahoo.com>
2024-09-17 13:32:05 +0200

    Commit basket service changes

--- MyShop/MyShop.DataAccess.SQL/Migrations/202409170922120_AddBasket.Designer.cs
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202409170922120_AddBasket.Designer.cs
@@ -7,13 +7,13 @@ namespace MyShop.DataAccess.SQL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class AddCustomer : IMigrationMetadata
+    public sealed partial class AddBasket : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(AddCustomer));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AddBasket));
         
         string IMigrationMetadata.Id
         {
-            get { return "201801032024566_AddCustomer"; }
+            get { return "202409170922120_AddBasket"; }
         }
         
         string IMigrationMetadata.Source
